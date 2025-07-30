d0d29c55f142248a108e6ce625002a17064ab8ef
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2025-07-30 10:27:18 +0200

    rag debug

--- rag_pdf/rag_analyzer.py
+++ rag_pdf/rag_analyzer.py
@@ -15,7 +15,7 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_chroma import Chroma
+#from langchain_chroma import Chroma
 import json
 from pathlib import Path
 
